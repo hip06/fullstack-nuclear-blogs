09b116da8ff1fbc6b32023eb1c8b139f334af307
kien19quang <kien19quang@gmail.com>
2022-07-20 20:11:21 +0700

    update and delete post

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -1,30 +1,30 @@
-import * as postService from '../services/postService'
+import * as postService from "../services/postService";
 // CREATE A POST
 export const createPost = async (req, res) => {
-    const { user, body } = req
-    try {
-        if (!user?.id) {
-            return res.status(404).json({
-                err: 1,
-                msg: 'Missing id user !'
-            })
-        }
-        if (user?.roleCode === 'ADMIN' || user?.roleCode === 'CRE') {
-            let response = await postService.createPostService(body, user.id)
-            return res.status(200).json(response)
-        } else {
-            return res.status(400).json({
-                err: 5,
-                msg: 'Require role Admin or Creator!'
-            })
-        }
-    } catch (error) {
-        return res.status(500).json({
-            err: -1,
-            msg: 'Fail at post-controller: ' + error
-        })
+  const { user, body } = req;
+  try {
+    if (!user?.id) {
+      return res.status(404).json({
+        err: 1,
+        msg: "Missing id user !",
+      });
     }
-}
+    if (user?.roleCode === "ADMIN" || user?.roleCode === "CRE") {
+      let response = await postService.createPostService(body, user.id);
+      return res.status(200).json(response);
+    } else {
+      return res.status(400).json({
+        err: 5,
+        msg: "Require role Admin or Creator!",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Fail at post-controller: " + error,
+    });
+  }
+};
 // GET ALL POST BY ID
 // export const getAllPostById = async (req, res) => {
 //     const { user } = req
@@ -54,30 +54,55 @@ export const createPost = async (req, res) => {
 
 // GET A POST
 export const getPost = async (req, res) => {
-    const { postId } = req.query
-    try {
-        if (!postId) return res.status(403).json({
-            err: 1,
-            msg: 'Missing postId !'
-        })
-        let response = await postService.getPostService(postId)
-        return res.status(200).json(response)
-    } catch (error) {
-        return res.status(500).json({
-            err: -1,
-            msg: 'Fail at post-controller: ' + error
-        })
-    }
-}
+  const { postId } = req.query;
+  try {
+    if (!postId)
+      return res.status(403).json({
+        err: 1,
+        msg: "Missing postId !",
+      });
+    let response = await postService.getPostService(postId);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Fail at post-controller: " + error,
+    });
+  }
+};
 // GET ALL POST
 export const getAllPost = async (req, res) => {
-    try {
-        let response = await postService.getAllPostService()
-        return res.status(200).json(response)
-    } catch (error) {
-        return res.status(500).json({
-            err: -1,
-            msg: 'Fail at post-controller: ' + error
-        })
-    }
-}
+  try {
+    let response = await postService.getAllPostService();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Fail at post-controller: " + error,
+    });
+  }
+};
+
+export const updatePostById = async (req, res) => {
+  try {
+    let response = await postService.updatePostByIdService(req.body);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Error from the server " + error,
+    });
+  }
+};
+
+export const deletePostById = async (req, res) => {
+  try {
+    let response = await postService.deletePostByIdService(req.query);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Error from the server " + error,
+    });
+  }
+};
--- server/src/services/postService.js
+++ server/src/services/postService.js
@@ -1,74 +1,178 @@
-import db from '../models'
-import { v4 as uuidv4 } from 'uuid';
+import db from "../models";
+import { v4 as uuidv4 } from "uuid";
 
 // POST A POST
-export const createPostService = (body, authorId) => new Promise(async (resolve, reject) => {
-    let data = { ...body, authorId, id: uuidv4() }
-    let bulkData = JSON.parse(data.tags)?.map(tag => ({ tag, specCode: body?.specCode }))
-    let editedBulkData = []
+export const createPostService = (body, authorId) =>
+  new Promise(async (resolve, reject) => {
+    let data = { ...body, authorId, id: uuidv4() };
+    let bulkData = JSON.parse(data.tags)?.map((tag) => ({
+      tag,
+      specCode: body?.specCode,
+    }));
+    let editedBulkData = [];
     try {
-        let existedTagBySpecCode = await db.Tag.findAll({ where: { specCode: body.specCode }, raw: true })
-        bulkData.forEach(t => {
-            let check = 0
-            if (existedTagBySpecCode.some(i => i.tag === t.tag)) {
-                check += 1
-            }
-            if (check === 0) editedBulkData = [...editedBulkData, { tag: t.tag, specCode: body.specCode }]
-        })
-        let [responsePost, responseTag] = await Promise.all([db.Post.create(data), editedBulkData && db.Tag.bulkCreate(editedBulkData)])
-        resolve({ err: 0, msg: 'Create OK', responsePost, responseTag })
+      let existedTagBySpecCode = await db.Tag.findAll({
+        where: { specCode: body.specCode },
+        raw: true,
+      });
+      bulkData.forEach((t) => {
+        let check = 0;
+        if (existedTagBySpecCode.some((i) => i.tag === t.tag)) {
+          check += 1;
+        }
+        if (check === 0)
+          editedBulkData = [
+            ...editedBulkData,
+            { tag: t.tag, specCode: body.specCode },
+          ];
+      });
+      let [responsePost, responseTag] = await Promise.all([
+        db.Post.create(data),
+        editedBulkData && db.Tag.bulkCreate(editedBulkData),
+      ]);
+      resolve({ err: 0, msg: "Create OK", responsePost, responseTag });
     } catch (error) {
-        reject(error)
+      reject(error);
     }
-})
+  });
 
 // GET A POST (PUBLIC)
-export const getPostService = (postId) => new Promise(async (resolve, reject) => {
+export const getPostService = (postId) =>
+  new Promise(async (resolve, reject) => {
     try {
-        let response = await db.Post.findOne({
-            where: { id: postId },
-            raw: true,
-            nest: true,
-            include: [
-                { model: db.User, attributes: ['firstName', 'lastName', 'avatar', 'avatarUrl'], as: 'author' },
-                { model: db.Specialization, attributes: ['value'], as: 'specialization' },
-            ]
-        })
-        response ? resolve({ err: 0, msg: 'OK', response }) : resolve({ err: 2, msg: 'Cannot get all post !' })
+      let response = await db.Post.findOne({
+        where: { id: postId },
+        raw: true,
+        nest: true,
+        include: [
+          {
+            model: db.User,
+            attributes: ["firstName", "lastName", "avatar", "avatarUrl"],
+            as: "author",
+          },
+          {
+            model: db.Specialization,
+            attributes: ["value"],
+            as: "specialization",
+          },
+        ],
+      });
+      response
+        ? resolve({ err: 0, msg: "OK", response })
+        : resolve({ err: 2, msg: "Cannot get all post !" });
     } catch (error) {
-        reject(error)
+      reject(error);
     }
-})
+  });
 
 // GET ALL POST BY ID
-export const getAllPostByIdService = (authorId) => new Promise(async (resolve, reject) => {
+export const getAllPostByIdService = (authorId) =>
+  new Promise(async (resolve, reject) => {
     try {
-        let response = await db.Post.findAll({
-            where: { authorId },
-            raw: true
-        })
-        response ? resolve({ err: 0, msg: 'OK', response }) : resolve({ err: 2, msg: 'Cannot get all post !' })
+      let response = await db.Post.findAll({
+        where: { authorId },
+        raw: true,
+      });
+      response
+        ? resolve({ err: 0, msg: "OK", response })
+        : resolve({ err: 2, msg: "Cannot get all post !" });
     } catch (error) {
-        reject(error)
+      reject(error);
     }
-})
+  });
 // GET ALL POST (PUBLIC)
-export const getAllPostService = () => new Promise(async (resolve, reject) => {
+export const getAllPostService = () =>
+  new Promise(async (resolve, reject) => {
     try {
-        let response = await db.Post.findAll({
-            raw: true,
-            nest: true,
-            include: [
-                { model: db.User, attributes: ['firstName', 'lastName', 'avatar', 'avatarUrl'], as: 'author' },
-                { model: db.Specialization, attributes: ['value'], as: 'specialization' },
-            ]
-        })
-        response ? resolve({ err: 0, msg: 'Create OK', response }) : resolve({ err: 2, msg: 'Cannot get all post !' })
+      let response = await db.Post.findAll({
+        raw: true,
+        nest: true,
+        include: [
+          {
+            model: db.User,
+            attributes: ["firstName", "lastName", "avatar", "avatarUrl"],
+            as: "author",
+          },
+          {
+            model: db.Specialization,
+            attributes: ["value"],
+            as: "specialization",
+          },
+        ],
+      });
+      response
+        ? resolve({ err: 0, msg: "Create OK", response })
+        : resolve({ err: 2, msg: "Cannot get all post !" });
     } catch (error) {
-        reject(error)
+      reject(error);
     }
-})
+  });
 
 // UPDATE A POST
+export const updatePostByIdService = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.id) {
+        resolve({
+          err: 1,
+          msg: "Missing parameter",
+        });
+      } else {
+        await db.Post.update(
+          {
+            title: data.title,
+            tags: data.tags,
+            thumbnailUrl: data.thumbnailUrl,
+            mdContent: data.mdContent,
+          },
+          {
+            where: { id: data.id },
+          }
+        );
 
+        let allPost = [];
+
+        allPost = await db.Post.findAll({
+          where: { authorId: data.authorId },
+        });
+
+        resolve({
+          err: 0,
+          msg: "OK",
+          data: allPost,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 // DELETE A POST
+export const deletePostByIdService = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.postId || !data.authorId) {
+        resolve({
+          err: 1,
+          msg: "Missing parameter",
+        });
+      } else {
+        await db.Post.destroy({
+          where: { id: data.postId },
+        });
+
+        let allPost = await db.Post.findAll({
+          where: { authorId: data.authorId },
+        });
+
+        resolve({
+          err: 0,
+          msg: "OK",
+          data: allPost,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
